61d606a7207372e04b971725170f735f69567161
onurrbl <onurer.bil12@gmail.com>
2021-05-07 00:24:01 +0300

    Created models

--- todocomp/models.py
+++ todocomp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
+class ToDo(models.Model):
+    user = models.ForeignKey(get_user_model(),null=True, on_delete=models.SET_NULL,related_name='todos')
+    todo_title = models.CharField(max_length=200, null=True ,blank=True)
+    todo_parameter = models.CharField(max_length=200, null=True, blank=True)
+    expire_date = models.DateField(null=True, blank=True)
+    
--- todocomp/views.py
+++ todocomp/views.py
@@ -3,11 +3,14 @@ from django.http import (
     HttpResponse,
     
 )
+from django.contrib.auth import get_user_model
 # Create your views here.
 
 
 
 def homeView(request,*args,**kwargs):
+    user = get_user_model()
+    obj = request.user.todos.all()
+    context = { "object" : obj}
 
-
-    return render(request,'base.html', { } )
+    return render(request,'base.html', context)
